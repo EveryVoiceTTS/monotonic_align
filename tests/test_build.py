e48474b03f64a650cb67300995bbc427628eac56
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-05-27 08:55:50 -0400

    test: make test files directly callable to minimal test deps

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,5 +1,12 @@
-from monotonic_align import VERSION
 from importlib.metadata import version
+
+from monotonic_align import VERSION
+
 
 def test_version_in_sync():
     assert VERSION == version("ilt-monotonic-align")
+
+
+if __name__ == "__main__":
+    test_version_in_sync()
+    print("OK")
--- tests/test_max_path.py
+++ tests/test_max_path.py
@@ -1,5 +1,6 @@
 import torch
-from monotonic_align import maximum_path, mask_from_lens
+
+from monotonic_align import mask_from_lens, maximum_path
 
 
 def test_maximum_path_eg1():
@@ -13,10 +14,10 @@ def test_maximum_path_eg1():
 
 def test_maximum_path_eg2():
     # Example 2
-    B = 16   # batch_size
-    S = 45   # max_symbol_len
+    B = 16  # batch_size
+    S = 45  # max_symbol_len
     T = 500  # max_mel_len
-    M = 80   # num_mels
+    M = 80  # num_mels
 
     symbol_embs = torch.randn(B, S, M)
     symbol_lens = torch.randint(1, S, size=[B])
@@ -24,9 +25,15 @@ def test_maximum_path_eg2():
     mels = torch.randn(B, T, M)
     mel_lens = torch.randint(1, T, size=[B])
 
-    similarity = - (symbol_embs.unsqueeze(2) - mels.unsqueeze(1)).pow(2).sum(-1)
+    similarity = -(symbol_embs.unsqueeze(2) - mels.unsqueeze(1)).pow(2).sum(-1)
     mask_ST = mask_from_lens(similarity, symbol_lens, mel_lens)
     alignments = maximum_path(similarity, mask_ST)
     print(alignments)
     assert isinstance(alignments, torch.Tensor)
     assert alignments.size() == torch.Size([16, 45, 500])
+
+
+if __name__ == "__main__":
+    test_maximum_path_eg1()
+    test_maximum_path_eg2()
+    print("OK")
